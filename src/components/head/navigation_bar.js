bd233a32cdbea67250916f08be4360559ab50869
Jerry Chang <therealjerrycan@gmail.com>
2016-11-08 20:17:03 -0800

    added searchbar toggle for navigation bar

--- src/components/head/navigation_bar.js
+++ src/components/head/navigation_bar.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NavigationBar = (props) => {
-    return <nav className="navbar navbar-default visible-xs">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <a className="navbar-brand" href="#">
-                        <p className="logo-title">Marvelous!</p>
-                    </a>
-                    <div className="options pull-right">
-                        <span 
-                            className="glyphicon glyphicon-search padding-right-25" 
-                            onClick={props.toggle}
-                        ></span>
-                        <span 
-                            className="glyphicon glyphicon-option-vertical" 
-                            onClick={props.toggle}
-                        ></span>
+export default class NavigationBar extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSearchBar: false,
+            inputText: ''
+        }
+        this.toggleSearchBar = this.toggleSearchBar.bind(this);
+    }
+
+    toggleSearchBar() {
+        this.setState({
+            showSearchBar: ! this.state.showSearchBar
+        })
+    }
+
+    render() {
+        return (<nav className="navbar navbar-default visible-xs">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">
+                            <p className="logo-title">Marvelous!</p>
+                        </a>
+                        <div className="options pull-right">
+                            <span 
+                                className="glyphicon glyphicon-search padding-right-25" 
+                                onClick={this.toggleSearchBar}
+                            ></span>
+                            <span 
+                                className="glyphicon glyphicon-option-vertical" 
+                                onClick={this.props.toggle}
+                            ></span>
+                        </div>
                     </div>
+                    <input 
+                        value={this.state.inputValue}
+                        className={`custom-search hide ${ this.state.showSearchBar ? "show" : "" }`} 
+                        type="text" /> 
                 </div>
-            </div>
-        </nav>
+            </nav>
+        );
+    }
 }
-
-export default NavigationBar;
